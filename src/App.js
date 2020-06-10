d0583afe66d3c62dcde487f78ad8c15dccfb0848
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-06-10 14:47:28 -0300

    Feedback page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import logo from './trivia.png';
 import './App.css';
+import Feedback from './components/feedback';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
+    <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>SUA VEZ</p>
+        </header>
     </div>
   );
 }
