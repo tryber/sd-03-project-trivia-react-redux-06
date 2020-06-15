3bc3b597b6e2172878a205178b6eee7ac10a601a
tamagoshii <estelaap_martins@hotmail.com>
2020-06-15 17:07:23 -0300

    test pricture ranking

--- src/components/RankingPage/Ranking.js
+++ src/components/RankingPage/Ranking.js
@@ -3,6 +3,14 @@ import { Link } from 'react-router-dom';
 
 class Ranking extends React.Component {
   static rankingList() {
+    const gravatarPlayer = (picture) => (
+      <img
+        data-testid="header-profile-picture"
+        src={picture}
+        alt="Gravatar"
+      />
+    );
+
     const getUser = JSON.parse(localStorage.getItem('ranking')) || [];
     const sortedUsers = getUser.sort((a, b) => {
       if (a.score > b.score) return -1;
@@ -12,10 +20,11 @@ class Ranking extends React.Component {
 
     return (
       <ul>
-        {sortedUsers.map(({ name, score }, indice) => {
+        {sortedUsers.map(({ name, score, picture }, indice) => {
           const index = indice + 1;
           return (
             <li>
+              {gravatarPlayer(picture)}
               <span data-testid={`player-name-${index}`}>
                 {`${name}`}
               </span>
