a663ab58a4bc563c0027b61e78429e726abc3d27
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-06-22 10:45:07 -0300

    CodeClimate check

--- src/components/SettingsPage/Settings.js
+++ src/components/SettingsPage/Settings.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
 class Settings extends React.Component {
-  // lll
   render() {
     return (
-      <div>
-        <h1 data-testid="settings-title">Configurações</h1>
+      <div style={{ margin: 'auto', textAlign: 'center' }}>
+        <h1 style={{ color: 'yellow' }} data-testid="settings-title">Configurações</h1>
       </div>
     );
   }
